9effab3f5b45860a86b9d8c67edc27b9f44cf34b
kcz358 <92624596+kcz358@users.noreply.github.com>
2023-10-19 20:13:06 +0800

    Fix tensorboard issue

--- train.py
+++ train.py
@@ -11,6 +11,8 @@ from torch.utils.tensorboard import SummaryWriter
 from datasets import flowers102
 from models import resnet50
 
+EPOCHS = 5
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description="A script that takes dataset path, model saving path, and tensorboard log path as arguments.")
 
@@ -35,14 +37,10 @@ def train(epoch, train_dataloader, val_dataloader, writer):
         loss.backward()
         optimizer.step()
         train_losses.append(loss.item())
-        writer.add_scalar("Loss/train", epoch, loss.item())
         
         pred = torch.argmax(F.softmax(loss, dim = 0))
         acc = (pred == labels).sum()
         train_accuracies.append(acc)
-        writer.add_scalar("Accuracy/train", epoch, acc)
-        
-        
     
     model.eval()
     val_losses = []
@@ -55,16 +53,19 @@ def train(epoch, train_dataloader, val_dataloader, writer):
         loss = criterion(output, labels)
         
         val_losses.append(loss.item())
-        writer.add_scalar("Loss/val", epoch, loss.item())
         
         pred = torch.argmax(F.softmax(loss, dim = 0))
         acc = (pred == labels).sum()
         val_accuracies.append(acc)
-        writer.add_scalar("Accuracy/val", epoch, acc)
+        
     train_losses = torch.tensor(train_losses, dtype=torch.float32)
     train_accuracies = torch.tensor(train_accuracies, dtype=torch.float32)
     val_losses = torch.tensor(val_losses, dtype=torch.float32)
     val_accuracies = torch.tensor(val_accuracies, dtype=torch.float32)
+    writer.add_scalar("Loss/train", epoch, train_losses.mean())
+    writer.add_scalar("Accuracy/train", epoch, train_accuracies.mean())
+    writer.add_scalar("Loss/val", epoch, val_losses.mean())
+    writer.add_scalar("Accuracy/val", epoch, val_accuracies.mean())
     return train_losses.mean(), train_accuracies.mean(), val_losses.mean(), val_accuracies.mean()
 
 def test(epoch, test_dataloader, writer):
@@ -84,10 +85,10 @@ def test(epoch, test_dataloader, writer):
         pred = torch.argmax(F.softmax(loss, dim = 0))
         acc = (pred == labels).sum()
         test_accuracies.append(acc)
-        writer.add_scalar("Accuracy/test", epoch, acc)
     
     test_losses = torch.tensor(test_losses, dtype=torch.float32)
     test_accuracies = torch.tensor(test_accuracies, dtype=torch.float32)
+    writer.add_scalar("Accuracy/test", epoch, test_accuracies.mean())
     
     return test_losses.mean(), test_accuracies.mean()
         
@@ -121,12 +122,12 @@ if __name__ == '__main__':
     formatted_time = current_time.strftime("%Y-%m-%d-%H:%M:%S").replace("-", "_").replace(":", "-")
     writer = SummaryWriter(log_dir=args.tensorboard_log_path + "/{}_{}_{}".format(model.name, train_dataset.name, formatted_time))
     
-    for epoch in range(1,51):
+    for epoch in range(1,EPOCHS+1):
         train_losses, train_accuracies, val_losses, val_accuracies = train(epoch, train_dataloader, val_dataloader, writer)
         test_losses, test_accuracies = test(epoch, test_dataloader, writer)
         
         if(epoch % 5 == 0):
-            print(f"Epoch [{epoch}/{50}] : Valid Acc {val_accuracies}, Test Acc {test_accuracies}")
+            print(f"Epoch [{epoch}/{EPOCHS}] : Valid Acc {val_accuracies}, Test Acc {test_accuracies}")
             state_dict = {
                 'epoch' : epoch,
                 'state_dict' : model.state_dict(),
