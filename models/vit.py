a89e21199abb742e82865096df81bfbfd994b9b1
Shi Zhengyu <shizhengyu93@hotmail.com>
2023-10-23 17:29:09 +0800

    feat: ViT pending test

--- models/vit.py
+++ models/vit.py
@@ -6,7 +6,7 @@ import torch
 
 from .base_model import BaseModel
 
-default_transformation = transforms.Compose([
+default_transformation = transforms.Compose([ # (conv_proj): Conv2d(3, 768, kernel_size=(16, 16), stride=(16, 16))
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
@@ -19,18 +19,16 @@ class ViT(BaseModel):
                  weights: str = None,
                  transformation=default_transformation):
         super().__init__(model_name)
-        self.encoder = torch.hub.load('pytorch/vision', 'vit_b_16', pretrained=False)
-        self.fc = nn.Linear(self.encoder.head.in_features, num_classes)
+        self.encoder = models.vit_b_16(weights = weights, progress = True)
+        self.encoder.heads[0] = nn.Linear(self.encoder.heads[0].in_features, num_classes) # originally (head): Linear(in_features=768, out_features=1000, bias=True)
         self.transformation = transformation
+        print(self.encoder)
 
     def forward(self, x):
-        x = self.encoder(x)
-        x = self.fc(x)
-        return x
+        return self.encoder(x)
 
     def to(self, device):
         self.encoder.to(device)
-        self.fc.to(device)
 
     def parameters(self, recurse: bool = True) -> Iterator[Parameter]:
-        return list(self.encoder.parameters(recurse)) + list(self.fc.parameters())
+        return self.encoder.parameters(recurse)
