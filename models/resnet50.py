43e82cb7bb210d7aee5a432627458448ca58390f
kcz358 <92624596+kcz358@users.noreply.github.com>
2023-11-02 10:01:34 +0800

    Adding image retrieval task

--- models/resnet50.py
+++ models/resnet50.py
@@ -49,6 +49,12 @@ class ResNet50(BaseModel):
 
         self.encoder = nn.Sequential(*layers)
         self.pooling = load_pooling(pool_name=pooling)
+        self.embedding_head = nn.Sequential(
+            nn.Linear(2048, 1024),
+            nn.ReLU(),
+            nn.Linear(1024,2048)
+        )
+        self.embedding_size = 2048
         self.cls_head = nn.Sequential(
             nn.Linear(model.fc.in_features, num_classes),
             nn.Softmax(dim = 1)
@@ -62,18 +68,28 @@ class ResNet50(BaseModel):
         features = self.encoder(x)
         # (B, 2048, 7, 7) --> (B, 2048, 1, 1) --> (B, 2048)
         features = self.pooling(features).flatten(1)
+        embedding = self.embedding_head(features)
         output = self.cls_head(features)
         if self.feature_extraction:
-            return output, features
+            return output, embedding
         else:      
             return output, None
     
     def to(self, device):
         self.encoder.to(device)
         self.cls_head.to(device)
+        self.embedding_head.to(device)
         # Gem is a trainable pooling layer
         if self.pooling.name != 'avgpooling' or self.pooling.name != 'sumpooling':
             self.pooling.to(device)
     
     def parameters(self, recurse: bool = True) -> Iterator[Parameter]:
-        return itertools.chain(self.encoder.parameters(recurse), self.cls_head.parameters(recurse))
+        if self.pooling.name != 'avgpooling' or self.pooling.name != 'sumpooling':
+            return itertools.chain(self.encoder.parameters(recurse), 
+                               self.cls_head.parameters(recurse),
+                               self.pooling.parameters(recurse),
+                               self.embedding_head.parameters(recurse))
+
+        return itertools.chain(self.encoder.parameters(recurse), 
+                               self.cls_head.parameters(recurse),
+                               self.embedding_head.parameters(recurse))
--- utils/transformation_constant.py
+++ utils/transformation_constant.py
@@ -26,7 +26,7 @@ resnet50_standard_augmentation = transforms.Compose([
 random_augmentation = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
-    transforms.RandAugment(),
+    transforms.RandAugment(magnitude=15),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
